df1337a7036a2823c0897b9847f0e56fa6dc92c7
Andres Cifuentes <aacifuentes@gmail.com>
2017-04-06 15:40:40 -0500

    created functional logger middleware

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,37 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import initialView from './view/initial-view.js'
+// import feedView from './view/feed-view.js'
+// import loginView from './view/login-view.js'
+// import signupView from './view/signup-view.js'
+import logger from './logger_middleware.js'
 export default function app() {
+  const url = 'https://api.backendless.com/v1/data/tw_clone;'
+
+  const initialState = {
+    user: null,
+    data: null,
+    view: initialView
+  }
+
+  const reducer = function(currentState, action){
+    if (currentState === undefined){
+      return initialState;
+    }
+    switch(action.type){
+      default:
+        return currentState;
+    }//end of switch
+  }//end of reducer
+
+  const store = createStore(reducer, applyMiddleware(logger));
+
+  const render = function(){
+    let state = store.getState();
+    $('#app').html(state.view(store));
+  }//end of render
+
+  store.subscribe(render);
+  store.dispatch({type: "NOOP"});
 
 
 
---
+++ app/scripts/view/initial-view.js
@@ -0,0 +1,6 @@
+export default function initialView (store){
+  let $html = $(`
+    <h1>hello</h1>
+    `)
+  return $html
+}//end of export
